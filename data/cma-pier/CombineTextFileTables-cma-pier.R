12ae51cf8e58c857830140c06fa86ff20b33a12c
Ryan Hartnett <rynocean@gmail.com>
2020-12-24 16:45:52 -0800

    Pulling together the rest of Cal Maritime 2020 data into one R Data file.

--- data/cma-pier/CombineTextFileTables-cma-pier.R
+++ data/cma-pier/CombineTextFileTables-cma-pier.R
@@ -1,12 +1,20 @@
+library(dplyr)
+library(lubridate)
+library(here)
+
 
 #combining data from text files
 
-setwd("C:/Users/Ryan/Desktop/2020 season summary/data/cma-pier")
+setwd(here())
+
+getwd()
+
+setwd(here("data", "cma-pier"))
+
+getwd()
+
 
 rm(list = ls())
-
-library(dplyr)
-library(lubridate)
 
 # ====================================================================================================================================
 #   Date     Time  Temp SpCond   Cond  Resist   TDS    Sal   Press   Depth    pH      pH   Chl   Chl Turbid+ ODOsat    ODO Battery
@@ -16,59 +24,105 @@ library(lubridate)
 
 list.files()
 
-# [1] "CMA_YSI_20191108-20191210.csv"    "CMA_YSI_20191210-20200103.csv"    "CMA_YSI_20200103-2020117.csv"    
-# [4] "CMA_YSI_20200117-20200206.csv"    "CMA_YSI_20200117.csv"             "CMA_YSI_20200206-20200221.csv"   
-# [7] "CMA_YSI_20200221-20200402.csv"    "CMA_YSI_20200403-20200514.csv"  "CMA_YSI_20200514-20200710.csv"
-
+file1 <- "CMA_YSI_20191108-20191210.csv"   
+file2 <- "CMA_YSI_20191210-20200103.csv"   
+file3 <- "CMA_YSI_20200103-2020117.csv"    
+file4 <- "CMA_YSI_20200117-20200206.csv"   
+file5 <- "CMA_YSI_20200117.csv"            
+file6 <- "CMA_YSI_20200206-20200221.csv"   
+file7 <- "CMA_YSI_20200221-20200402.csv"   
+file8 <- "CMA_YSI_20200403-20200514.csv"   
+file9 <- "CMA_YSI_20200514-20200710.csv"   
+file10 <- "CMA_YSI_20200714-20200820.csv"   
+file11 <- "CMA_YSI_20200821-20200923.csv"   
+file12 <- "CMA_YSI_20200924-20201014.csv"   
+file13 <- "CMA_YSI_20201015-20201102.csv"   
+file14 <- "CMA_YSI_20201114-20201215.csv"   
+file15 <- "CMA_YSI_20201216-20201219.csv"   
 
 #read data files and match var names ######################################
 
-df1 = read.csv("CMA_YSI_20191108-20191210.csv",
+df1 = read.csv(file1,
                header=T, stringsAsFactors=F, sep=",")
 df1 <- df1[-1,]
 
 
-df2 = read.csv("CMA_YSI_20191210-20200103.csv",
+df2 = read.csv(file2,
                header=T, stringsAsFactors=F, sep=",")
 df2 <- df2[-1,]
 
 
-df3 = read.csv("CMA_YSI_20200103-2020117.csv",
+df3 = read.csv(file3,
                header=T, stringsAsFactors=F, sep=",")
 df3 <- df3[-1,]
 
 
-df4 = read.csv("CMA_YSI_20200117-20200206.csv",
+df4 = read.csv(file4,
                header=T, stringsAsFactors=F, sep=",")
 df4 <- df4[-1,]
 
 
-df5 = read.csv("CMA_YSI_20200117.csv",
+df5 = read.csv(file5,
                header=T, stringsAsFactors=F, sep=",")
 df5 <- df5[-1,]
 
 
-df6 = read.csv("CMA_YSI_20200206-20200221.csv",
+df6 = read.csv(file6,
                header=T, stringsAsFactors=F, sep=",")
 df6 <- df6[-1,]
 
 
-df7 = read.csv("CMA_YSI_20200221-20200402.csv",
+df7 = read.csv(file7,
                header=T, stringsAsFactors=F, sep=",")
 df7 <- df7[-1,]
 
 
-df8 = read.csv("CMA_YSI_20200403-20200514.csv",
+df8 = read.csv(file8,
                header=T, stringsAsFactors=F, sep=",")
 df8 <- df8[-1,]
 
 
-df9 = read.csv("CMA_YSI_20200514-20200710.csv",
+df9 = read.csv(file9,
                header=T, stringsAsFactors=F, sep=",")
 df9 <- df9[-1,]
 
 
-df <- bind_rows(df1, df2, df3, df4, df5, df6, df7, df8, df9, .id= NULL)
+df10 = read.csv(file10,
+               header=T, stringsAsFactors=F, sep=",")
+df10 <- df10[-1,]
+
+
+df11 = read.csv(file11,
+               header=T, stringsAsFactors=F, sep=",")
+df11 <- df11[-1,]
+
+
+df12 = read.csv(file12,
+               header=T, stringsAsFactors=F, sep=",")
+df12 <- df12[-1,]
+
+
+df13 = read.csv(file13,
+               header=T, stringsAsFactors=F, sep=",")
+df13 <- df13[-1,]
+
+
+df14 = read.csv(file14,
+               header=T, stringsAsFactors=F, sep=",")
+df14 <- df14[-1,]
+
+
+df15 = read.csv(file15,
+               header=T, stringsAsFactors=F, sep=",")
+df15 <- df15[-1,]
+
+# doesnt work. may need grep?
+# bind_list <- paste0("df", c(1:15))
+# 
+# df <- bind_rows(bind_list, .id= NULL)
+
+df <- bind_rows(df1, df2, df3, df4, df5, df6, df7, df8, df9,
+                df10, df11, df12, df13, df14, df15, .id= NULL)
 
 rm(list=setdiff(ls(), c("df")))
 
@@ -122,15 +176,19 @@ str(df)
 
 cma_2020 <- select(df, datetime, everything())
 
+rm(df)
 
-save(cma_2020, file = "CMA_YSI_20191108-20200710.RData")
+save(cma_2020, file = "CMA_YSI_20191108-20201219.RData")
 
 rm(list=ls())
 
-load(file = "CMA_YSI_20191108-20200710.RData")
+load(file = "CMA_YSI_20191108-20201219.RData")
 
 str(cma_2020)
 
-write.csv(cma_2020, file = "CMA_YSI_20191108-20200710.csv", row.names = F)
 
+
+# write.csv(cma_2020, file = "CMA_YSI_20191108-20200710.csv", row.names = F)
+
+rm(cma_2020)
 
