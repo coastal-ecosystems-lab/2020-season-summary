76bd3188c210c423815c0d34a03e4e4310149ade
Ryan Hartnett <rynocean@gmail.com>
2020-12-24 16:06:01 -0800

    Read in updated EOS Met Station data from text files and combined into an RData file for analysis

--- data/eos-met/CombineTextFileTables.eos-met.R
+++ data/eos-met/CombineTextFileTables.eos-met.R
@@ -1,13 +1,20 @@
+library(dplyr)
+library(lubridate)
+library(here)
+
 
 #combining data from text files
 
-setwd("C:/Users/Ryan/Desktop/2020 season summary/data/eos-met")
+setwd(here())
+
+getwd()
+
+setwd(here("data", "eos-met"))
+
+getwd()
+
 
 rm(list = ls())
-
-library(dplyr)
-library(lubridate)
-
 
 # ====================================================================================================================================
 #   Date     Time  Temp SpCond   Cond  Resist   TDS    Sal   Press   Depth    pH      pH   Chl   Chl Turbid+ ODOsat    ODO Battery
@@ -19,6 +26,12 @@ list.files()
 
 file1 <- "RTC_MET_20191120-20191227.txt"   
 file2 <- "RTC_MET_20191227-20200515.txt"
+file3 <- "RTC_MET_20200515-20200620.txt"  
+file4 <- "RTC_MET_20200620-20200802.txt"  
+file5 <- "RTC_MET_20200813-20200902.txt"  
+file6 <- "RTC_MET_20200902-20201219.txt" 
+
+
 header <- read.table(file1, as.is = T, skip = 1,nrows = 1, header = F, sep = ",")
 
 # # read data by ERDDAP -----------------------------
@@ -107,6 +120,40 @@ colnames(df2) <- header[1,]
 
 str(df2)
 
+
+
+df3 <- read.table(file3, sep = ",", as.is = T, skip = 5, header = F)
+
+# assigns columns names pulled from header "file"
+colnames(df3) <- header[1,]
+
+str(df3)
+
+
+df4 <- read.table(file4, sep = ",", as.is = T, skip = 5, header = F)
+
+# assigns columns names pulled from header "file"
+colnames(df4) <- header[1,]
+
+str(df4)
+
+
+df5 <- read.table(file5, sep = ",", as.is = T, skip = 5, header = F)
+
+# assigns columns names pulled from header "file"
+colnames(df5) <- header[1,]
+
+str(df5)
+
+
+df6 <- read.table(file6, sep = ",", as.is = T, skip = 5, header = F)
+
+# assigns columns names pulled from header "file"
+colnames(df6) <- header[1,]
+
+str(df6)
+
+
 # getting variable names
 # head(df1)
 # 
@@ -137,7 +184,7 @@ str(df2)
 #          rain = 'V13')
 # 
 
-df <- bind_rows(df1, df2, .id= NULL)
+df <- bind_rows(df1, df2, df3, df4, df5, df6, .id= NULL)
 
 rm(list=setdiff(ls(), c("df")))
 
@@ -146,7 +193,7 @@ df$datetime <- ymd_hms(df$TIMESTAMP, tz = "UTC")
 str(df)
 str(df$datetime)
 
-df1$date <- date(df1$datetime)
+df$date <- date(df$datetime)
 
 #not needed with above line
 #df1$datetime <- as.POSIXct(df1$datetime, format = "%Y/%m/%d %H:%M:%S")
@@ -159,14 +206,17 @@ df <- df %>%
 # #change all character columns to numeric
 # df1 %<>% mutate_if(is.character,as.numeric)
 
-df <- select(df, datetime, everything())
+eos_met_2020 <- select(df, datetime, date, TIMESTAMP, everything())
 
+rm(df)
 
-save(df, file = "EOS_MET_20191201-20200515.RData")
+#save(df, file = "EOS_MET_20191201-20200515.RData")
+
+save(eos_met_2020, file = "EOS_MET_20191120-20201219.RData")
 
 rm(list = ls())
 
-load(file = "EOS_MET_20191201-20200515.RData")
+load(file = "EOS_MET_20191120-20201219.RData")
 
 rm(list = ls())
 
